8fffec1c1913b7921db5b47516dd921a73fecd70
Nicholas Stakoun <nicholas@stakoun.com>
2020-07-18 21:31:08 -0400

    Built plugin

--- src/com/stakoun/nicholas/replaceblock/ReplaceBlock.java
+++ src/com/stakoun/nicholas/replaceblock/ReplaceBlock.java
@@ -3,5 +3,13 @@ package com.stakoun.nicholas.replaceblock;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ReplaceBlock extends JavaPlugin {
-    // TODO
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
